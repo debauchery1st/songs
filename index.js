a7e8627db5153048678aeb85c7dd568b36cd1a8b
debauchery1st <trevor.j.martin@gmail.com>
2020-02-13 15:48:55 -0500

    videoURL

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+require("dotenv").config();
 const express = require("express");
 const server = express();
 server.use(express.json());
 const port = process.env.PORT || 4003;
 
-const initSongs = [{ id: 1, name: "Final Countdown" }];
+const initSongs = [
+  {
+    id: 1,
+    artist: "Europe",
+    name: "Final Countdown",
+    videoURL: "https://youtu.be/9jK-NcRmVcw",
+    test: process.env.OS
+  },
+  {
+    id: 2,
+    artist: "Heart",
+    name: "Barracuda",
+    videoURL: "https://youtu.be/PeMvMNpvB5M"
+  },
+  {
+    id: 3,
+    artist: "Kansas",
+    name: "Carry on Wayward Son",
+    videoURL: "https://youtu.be/P5ZJui3aPoQ"
+  },
+  {
+    id: 4,
+    artist: "Electric Light Orchestra",
+    name: "Mr. Blue Sky",
+    videoURL: "https://youtu.be/aQUlA8Hcv4s"
+  },
+  {
+    id: 5,
+    artist: "Survivor",
+    name: "Eye of the Tiger",
+    videoURL: "https://youtu.be/btPJPFnesV4"
+  }
+];
 
 server.get("/", (req, res) => {
   res.status(200).json(initSongs);
