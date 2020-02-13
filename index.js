725ec7309f1e5ef1ca1d08ab0c592442e7c3f2f9
debauchery1st <trevor.j.martin@gmail.com>
2020-02-13 13:30:06 -0500

    dynamic port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
 server.use(express.json());
-const port = 4003;
+const port = process.env.PORT || 4003;
 
 const initSongs = [{ id: 1, name: "Final Countdown" }];
 
